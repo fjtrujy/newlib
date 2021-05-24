c8b01dc32cbdcbcf7e82c139c289033c6903983f
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-05-24 12:41:42 +0200

    Update dirent.h

--- newlib/libc/sys/psp/sys/dirent.h
+++ newlib/libc/sys/psp/sys/dirent.h
@@ -2,8 +2,12 @@
 #define _SYS_DIRENT_H
 
 typedef struct __dirdesc {
-	int	dd_fd;
-	char	*dd_buf;
+	int dd_fd;		/* directory file */
+    int dd_loc;		/* position in buffer */
+    int dd_seek;
+    char *dd_buf;	/* buffer */
+    int dd_len;		/* buffer length */
+    int dd_size;	/* amount of data in buffer */
 } DIR;
 
 # define __dirfd(dp)	((dp)->dd_fd)
@@ -17,7 +21,10 @@ typedef struct __dirdesc {
 
 struct dirent {
 	struct stat d_stat;
-	char d_name[MAXNAMLEN + 1];
+	unsigned long	d_fileno;
+	unsigned short	d_reclen;
+	unsigned short	d_namlen;
+	char		d_name[MAXNAMLEN + 1];
 };
 
 #endif
